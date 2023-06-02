950e40bee86adba1e53e7c132dd8643b60a79756
trinhtuanvu <vutuantrinh2000@gmail.com>
2023-06-02 13:59:39 +0700

    init bash cript

---
+++ run.sh
@@ -0,0 +1,6 @@
+python3 app.py \
+--model_path "./model_repository/w2v2_ckpt/best_model.tar" \
+--lm_path "./model_repository/language_model/4gram_small.arpa" \
+--device 0 \
+--port 1435 \
+--use_language_model
